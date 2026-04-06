be02a989d99177a592c24980164fdee7dd087566
JinjuRuby <ronnie0304@naver.com>
2026-04-06 17:27:17 +0900

    feat: User 엔티티 개선
    - id 필드 DB 컬럼명 수정(user_id)
    - email(unique, nullable), password(nullable) 제약조건 추가
    - balance 필드 기본값 설정

--- src/main/java/practice/budget_book/entity/User.java
+++ src/main/java/practice/budget_book/entity/User.java
@@ -14,12 +14,16 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
-    private Long balance;
+    @Builder.Default // Builder를 생성할 때 값을 지정하지 않는다면 null로 덧칠해버리기 때문에 이 애노테이션이 필요하다.
+    private Long balance = 0L;
 
 }
